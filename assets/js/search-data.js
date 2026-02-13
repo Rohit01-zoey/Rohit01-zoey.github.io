9613972bfb986e02f7975d26858a39da8ab1f097
Rohit01-zoey <55778017+Rohit01-zoey@users.noreply.github.com>
2026-02-13 18:21:48 +0000

    Deploying to gh-pages from @ Rohit01-zoey/Rohit01-zoey.github.io@6148f7ce821341f5fe36c25bf195852cedc06e1d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -483,7 +483,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%6F%68%69%74%76%61%72%74%61%6B%34@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
