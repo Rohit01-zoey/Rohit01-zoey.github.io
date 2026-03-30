cfaa89504916e2d6592dda2a3dada91dfbcdb0e1
Rohit01-zoey <55778017+Rohit01-zoey@users.noreply.github.com>
2026-03-30 19:37:18 +0000

    Deploying to gh-pages from @ Rohit01-zoey/Rohit01-zoey.github.io@4bbae3737336afad043e804877f872f8af0adf19 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "",
+          description: "A detailed overview of my academic background, research experience, publications, and technical skills.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
