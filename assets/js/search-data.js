0cd902e6e4f087ca31b9eb2991a171e76cafee38
Rohit01-zoey <55778017+Rohit01-zoey@users.noreply.github.com>
2026-03-30 17:31:58 +0000

    Deploying to gh-pages from @ Rohit01-zoey/Rohit01-zoey.github.io@0a70f580e303cd88999b272e074f25eae9963c86 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -82,13 +82,6 @@ ninja.data = [{
           window.open("mailto:%72%6F%68%69%74%6B%76%61%72%74%61%6B%34@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
